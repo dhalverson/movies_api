04eb99ea104d42bb1f63d3e3f593b3dc247b2f09
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-27 17:53:11 -0700

    Updates factories

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :rating do
     user_id { "MyString" }
-    rating { 1.5 }
+    rating { Faker::Number.between(from: 1, to: 5) }
     timestamp { "2021-02-27 16:18:55" }
     movie { nil }
   end
